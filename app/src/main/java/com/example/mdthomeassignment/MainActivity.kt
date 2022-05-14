5efd8c3428857fe14a7a63f36ab4eacd20066d84
Bhavana wagh <waghbhavana10@gmail.com>
2022-05-14 23:37:24 +0800

    Add navigation graph Add login and registration UI screens

---
+++ app/src/main/java/com/example/mdthomeassignment/MainActivity.kt
@@ -0,0 +1,16 @@
+
+package com.example.mdthomeassignment
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mdthomeassignment.ui.auth.AuthActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        finish();
+        startActivity(Intent(this,AuthActivity::class.java))
+    }
+}
---
+++ app/src/main/java/com/example/mdthomeassignment/ui/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.example.mdthomeassignment.ui.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mdthomeassignment.R
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
